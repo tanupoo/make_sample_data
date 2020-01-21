9d031aa41c8e03cedcc007ae68f267cf15a76b0c
tanupoo <github-shoichi@tanu.org>
2020-01-21 12:29:57 +0900

    added to show the result by networkx.

--- network.py
+++ network.py
@@ -7,16 +7,18 @@ def network_sample(node_profile=None,
         a set of { node_name : nb_edge }
         default is:
             { "A":10, "B":4, "C":3, "D":1, ..., "Z":1 }
+    return:
+        In the default profile, "A" has 10 edges from the differnt nodes.
     """
+    if random_seed is not None:
+        random.seed(random_seed)
+    #
     if node_profile is None:
         node_profile = {}
         node_list = [chr(_) for _ in range(ord("A"), ord("Z")+1)]
-        weight_list = [ 10, 4, 3 ] + [1 for _ in range(len(node_list)-3)]
+        weight_list = [ 10, 5, 3 ] + [1 for _ in range(len(node_list)-3)]
         for a,b in zip(node_list, weight_list):
             node_profile.update({a:b})
-    #
-    if random_seed is not None:
-        random.seed(random_seed)
     #
     sample = []
     node_list = node_profile.keys()
@@ -24,12 +26,23 @@ def network_sample(node_profile=None,
         peers = list(node_list).copy()
         peers.remove(n)
         for i in range(w):
-            sample.append((n, random.choice(peers)))
+            sample.append((random.choice(peers), n))
     #
     return sample
 
 if __name__ == "__main__":
-    n = network_sample({"A":5, "B":3, "C":-1})
-    print(n)
-    n = network_sample()
-    print(n)
+    import matplotlib.pyplot as plt
+    import networkx as nx
+    def show_result(edges):
+        G = nx.DiGraph()
+        G.add_edges_from(edges)
+        pos = nx.spring_layout(G)
+        nx.draw_networkx_nodes(G, pos)
+        nx.draw_networkx_edges(G, pos)
+        plt.show()
+    #
+    result = network_sample({"A":5, "B":3, "C":-1})
+    for i in result:
+        print(i)
+    show_result(result)
+    show_result(network_sample(random_seed=0))
